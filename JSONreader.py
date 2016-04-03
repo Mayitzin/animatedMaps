c1e3c04064abd6498abfcfbee0417f8c52ddd8c2
Mario Garcia <mayitzin@gmail.com>
2016-04-03 19:08:49 +0200

    Add JSON to dictionary decoder

--- JSONreader.py
+++ JSONreader.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
 """
 JSON reader
+
+@author: Mario Garcia
+www.mayitzin.com
 """
 
 import json
+import simplejson
+
+fileName = './data/mexbdy.json'
+
+# Read JSON trsing from file and convert into Dictionary
+with open(fileName, 'r') as f:
+	json_data = json.load(f)
+
+print "File",fileName,"is now",type(json_data)
