665b962d6e60fa205f65d2d784ed3c0622915b77
Mario Garcia <mayitzin@gmail.com>
2016-04-03 19:26:17 +0200

    Print a list of the States in the JSON file

--- JSONreader.py
+++ JSONreader.py
@@ -15,12 +15,20 @@ fileName = './data/mexbdy.json'
 with open(fileName, 'r') as f:
 	json_data = json.load(f)
 
-print "File",fileName,"is now",type(json_data)
+def printProperties(json_data, state_number=0):
+	print json_data['objects']['mexbdy']['geometries'][state_number]['properties'].keys()
+
+def listStates(json_data):
+	states_list = []
+	n = len(json_data['objects']['mexbdy']['geometries'])
+	for i in range(n):
+		states_list.append(json_data['objects']['mexbdy']['geometries'][i]['properties']['DESCRIP'])
+	return states_list
 
 print json_data.keys()
 
 print len(json_data['objects'])
 print json_data['objects'].keys()
 print json_data['objects']['mexbdy'].keys()
-# Print Properties of each State
-print json_data['objects']['mexbdy']['geometries'][0]['properties'].keys()
+print len(json_data['objects']['mexbdy']['geometries'])
+print listStates(json_data)
