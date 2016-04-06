500268672776cab17a8124a5cffcc0eec8bc7ecf
Mario Garcia <mayitzin@gmail.com>
2016-04-06 14:55:47 +0200

    List municipalities with a function

--- JSONreader.py
+++ JSONreader.py
@@ -14,28 +14,29 @@ fileName = './data/Municipios_2010_5A.json'
 
 # Read JSON trsing from file and convert into Dictionary
 with open(fileName, 'r') as f:
-	json_data = json.load(f)
+    json_data = json.load(f)
 
 def printProperties(json_data, state_number=0):
-	print json_data['objects']['mexbdy']['geometries'][state_number]['properties'].keys()
+    print json_data['objects']['mexbdy']['geometries'][state_number]['properties'].keys()
 
 def listStates(json_data):
-	states_list = []
-	n = len(json_data['objects']['mexbdy']['geometries'])
-	for i in range(n):
-		states_list.append(json_data['objects']['mexbdy']['geometries'][i]['properties']['DESCRIP'])
-	return states_list
+    states_list = []
+    n = len(json_data['objects']['mexbdy']['geometries'])
+    for i in range(n):
+        states_list.append(json_data['objects']['mexbdy']['geometries'][i]['properties']['DESCRIP'])
+    return states_list
 
-# # Print keys of mexbdy.json
-# print json_data.keys()
-# print len(json_data['objects'])
-# print json_data['objects'].keys()
-# print json_data['objects']['mexbdy'].keys()
-# print len(json_data['objects']['mexbdy']['geometries'])
+def listMunicipalities(json_data):
+    municipality_list = []
+    n = len(json_data['objects']['Municipios_2010_5A']['geometries'])
+    for i in range(n):
+        municipality_list.append(json_data['objects']['Municipios_2010_5A']['geometries'][i]['properties']['NOM_MUN'])
+    return municipality_list
+
+
 # # List States
 # print listStates(json_data)
 
-# Print keys of Municipios_2010_5A.json
-print json_data.keys()
-print len(json_data['objects'])
-print json_data['objects'].keys()
+# List Municipalities
+municList = listMunicipalities(json_data)
+print "There are", len(municList), "Municipalities listed"
